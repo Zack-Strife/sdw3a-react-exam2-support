632c2b983fef3cdd7a8dddafeec11bbb2f3ffdb0
Zack-Strife <hugo.capezzone@gmail.com>
2017-05-02 18:48:08 +0200

    fix

--- server/Api.js
+++ server/Api.js
@@ -9,7 +9,7 @@ const ProjectController = require('./controller/Project');
 const bodyParser = require('body-parser');
 const router = Router();
 
-// mongoose.connect(`mongodb:/${process.env.MLAB_DBUSER}:${process.env.MLAB_PASSWORD}@ds129281.mlab.com:29281/heroku_db`);
+mongoose.connect(`mongodb://${process.env.MLAB_DBUSER}:${process.env.MLAB_PASSWORD}@ds129281.mlab.com:29281/heroku_db`);
 
 router.use(bodyParser.json({'extended': true}));
 router.use(bodyParser.json());
@@ -18,17 +18,17 @@ router.get('/', (req, res) => {
   res.send('coucou api');
 });
 
-// router.get('/users', UserController.getUsers);
-// router.get('/user/:id', UserController.getUser);
-// router.post('/user', UserController.createUser);
-// router.put('/user/:id', UserController.updateUser);
-// router.delete('/user/:id', UserController.deleteUser);
+router.get('/users', UserController.getUsers);
+router.get('/user/:id', UserController.getUser);
+router.post('/user', UserController.createUser);
+router.put('/user/:id', UserController.updateUser);
+router.delete('/user/:id', UserController.deleteUser);
 
-// router.get('/projects', ProjectController.getProjects);
-// router.get('/project/:id', ProjectController.	getProject);
-// router.post('/project', ProjectController.createProject);
-// router.put('/project/:id', ProjectController.updateProject);
-// router.delete('/project/:id', ProjectController.deleteProject);
-// router.get('/user/:id/projects', ProjectController.getUserProjects);
+router.get('/projects', ProjectController.getProjects);
+router.get('/project/:id', ProjectController.	getProject);
+router.post('/project', ProjectController.createProject);
+router.put('/project/:id', ProjectController.updateProject);
+router.delete('/project/:id', ProjectController.deleteProject);
+router.get('/user/:id/projects', ProjectController.getUserProjects);
 
 module.exports = router;

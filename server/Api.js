558592bd00a20cd408d688f79612bfda51c7d6bd
Zack-Strife <hugo.capezzone@gmail.com>
2017-05-02 18:21:59 +0200

    add auth

--- server/Api.js
+++ server/Api.js
@@ -9,11 +9,7 @@ const ProjectController = require('./controller/Project');
 const bodyParser = require('body-parser');
 const router = Router();
 
-if(process.env.NODE_ENV == 'production') {
-  mongoose.connect(`mongodb://${process.env.MLAB_DBUSER}:${process.env.MLAB_DBPASSWORD}@${process.env.MLAB_DBURL}`);
-} else {
-  mongoose.connect('mongodb://localhost:27017/kickass');
-}
+mongoose.connect(`mongodb:/${process.env.MLAB_DBUSER}:${process.env.MLAB_PASSWORD}@ds129281.mlab.com:29281/heroku_db`);
 
 router.use(bodyParser.json({'extended': true}));
 router.use(bodyParser.json());
@@ -29,7 +25,7 @@ router.put('/user/:id', UserController.updateUser);
 router.delete('/user/:id', UserController.deleteUser);
 
 router.get('/projects', ProjectController.getProjects);
-router.get('/project/:id', ProjectController.getProject);
+router.get('/project/:id', ProjectController.	getProject);
 router.post('/project', ProjectController.createProject);
 router.put('/project/:id', ProjectController.updateProject);
 router.delete('/project/:id', ProjectController.deleteProject);
